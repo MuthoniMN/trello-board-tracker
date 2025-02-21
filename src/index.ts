7fb77457526283fdc4be6f0fb223f51d4cf269b4
Mitch <ndianguimichelle@gmail.com>
2025-02-21 07:04:42 +0300

    feat: setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import router from "./routes/";
+
+const app = express();
+
+app.use(cors());
+
+app.use('/', router);
+
+app.listen(5000, () => console.log("Server runnning on port 5000"));
---
+++ src/routes/index.ts
@@ -0,0 +1,26 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get('/integration.json', (req: any,res: any) => {
+  return res.json({
+    name: "Modify"
+  });
+});
+
+router.post('/trello/authorize', (req: any, res: any) => {
+
+});
+
+router.post('/target', (
+  req: any,
+  res: any
+) => {
+  return res.json({ status: 202, description: "Data received successfully!" })
+});
+
+router.post('/webhook', (req: any, res: any) => {
+
+});
+
+export default router;
