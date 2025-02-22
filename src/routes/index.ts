7e6b6f888c443014ff9e6f28bc937c92ce54aa83
Mitch <ndianguimichelle@gmail.com>
2025-02-22 14:19:59 +0300

    test: logging token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -24,7 +24,8 @@ router.post('/tick', async(
 ) => {
   const { settings } = req.body;
   console.log(settings);
-  const token = settings.find((setting: TSetting) => setting.label == "Trello API Token").default;
+  const token = settings.find((setting: TSetting) => setting.label == "Trello API Token").default || process.env.TRELLO_API_TOKEN;
+  console.log(token);
   const boards = settings.find((setting: TSetting) => setting.label == "Which Trello board would you like to track?").default.split(',');
   const today = new Date();
 
